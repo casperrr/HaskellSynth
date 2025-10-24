7b02cefa7202c8269a36c094b058f74cec859302
casperrr <casper.sebastien@gmail.com>
2025-10-24 15:43:41 +0100

    created a new output method using wav files

--- Synth/Util.hs
+++ Synth/Util.hs
@@ -1,21 +1,75 @@
 module Synth.Util where
 
-import Data.ByteString.Builder (floatLE, toLazyByteString)
+import Data.ByteString.Builder (floatLE, toLazyByteString, word32LE, word16LE, int16LE, string8)
 import Data.ByteString.Lazy (writeFile)
 import Prelude hiding (writeFile)
 import System.Process (runCommand)
-
-import Synth.Types ( Audio )
-import Synth (wave, sampleRate)
 import Text.Printf (printf)
+import Data.Int (Int16)
+import Data.Word (Word32)
+
+import Synth.Types ( Audio, Sample )
+import Synth (wave, sampleRate)
 
 ----------- File Creation -----------
 
-save :: FilePath -> Audio -> IO ()
-save filePath audio = writeFile filePath $ toLazyByteString $ foldMap floatLE audio
+-- Saves the audio as a raw 32-bit float little-endian file
+saveRaw :: FilePath -> Audio -> IO ()
+saveRaw filePath audio = writeFile filePath $ toLazyByteString $ foldMap floatLE audio
+
+playRaw :: Audio -> IO ()
+playRaw audio = do
+    saveRaw "output.bin" audio
+    _ <- runCommand $ printf "ffplay -showmode 1 -f f32le -ar %f %s" sampleRate "output.bin"
+    return ()
+
+-- Trying to write wav files
+
+-- saveWav :: FilePath -> Audio -> IO ()
+
+-- In order to use wav file, its convinient to use 16-Bit PCM instead of 32-Bit Float.
+-- To do this i need to:
+-- clip values to [-1.0, 1.0]
+-- Scale to Int-16 range [-32768, 32767]
+
+clamp :: Sample -> Sample
+clamp x = max (-1.0) (min 1.0 x)
+
+floatToInt16 :: Sample -> Int16
+floatToInt16 s = round (clamp s * 32767.0)
+
+saveWav :: FilePath -> Audio -> IO ()
+saveWav filePath samples = writeFile filePath $ toLazyByteString builder
+    where
+        builder = mconcat [
+            ---- RIFF Header ----
+            string8 "RIFF", -- ChunkID
+            word32LE (4 + (8 + 16) + (8 + dataBytes)), -- ChunkSize, Overall size of file 32-Int
+            string8 "WAVE", -- Format
+            ---- Format Chunk ----
+            string8 "fmt ", -- Subchunk1ID
+            word32LE 16, -- Subchunk1Size, PCM = 16
+            word16LE 1, -- AudioFormat, PCM = 1
+            word16LE 1, -- NumChannels, Mono = 1
+            word32LE sr, -- SampleRate
+            word32LE (sr * (16 `div` 8)), -- ByteRate = SampleRate * NumChannels * BitsPerSample/8
+            word16LE (16 `div` 8), -- BlockAlign = NumChannels * BitsPerSample/8
+            word16LE 16, -- BitsPerSample
+            ---- Data Chunk ----
+            string8 "data", -- Subchunk2ID
+            word32LE dataBytes, -- Subchunk2Size = NumSamples * NumChannels * BitsPerSample/8
+            foldMap (int16LE . floatToInt16) samples -- Actual sound data
+         ]
+
+        -- The size of the data section in bytes
+        dataBytes :: Word32
+        dataBytes = fromIntegral $ length samples * (16 `div` 8)
+
+        sr :: Word32
+        sr = round sampleRate
 
 play :: Audio -> IO ()
 play audio = do
-    save "output.bin" audio
-    _ <- runCommand $ printf "ffplay -showmode 1 -f f32le -ar %f %s" sampleRate "output.bin"
+    saveWav "output.wav" audio
+    _ <- runCommand $ printf "ffplay -showmode 1 -f wav %s" "output.wav"
     return ()
